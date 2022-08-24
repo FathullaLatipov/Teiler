b0f938aa2aaf053a869074880af509632dc0b2db
User <user@MacBook-Air-User.local>
2022-08-24 20:36:02 +0500

    add to cart in single product

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,6 @@ app_name = 'cart'
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('added/<int:product_id>/', views.single_cart_add, name='single_cart_add'),
     path('remove/<int:product_id>/', views.cart_remove, name='cart_remove')
 ]
--- cart/views.py
+++ cart/views.py
@@ -17,6 +17,16 @@ def cart_add(request, product_id):
         cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
     return redirect(request.GET.get('next', '/'))
 
+@require_POST
+def single_cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(ProductModel, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
 
 def cart_remove(request, product_id):
     cart = Cart(request)
